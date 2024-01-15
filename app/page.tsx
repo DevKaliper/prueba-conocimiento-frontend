e2e7dca26a33ee5eec22ee4717f5fab0fbbb5f36
Dev Kaliper <gxlthz@gmail.com>
2024-01-15 18:05:26 -0500

    footer added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { InputInterface } from '@/types/types';
 import { useState,useEffect } from 'react';
 
 
+
 export default function Home() {
    // Inicializar el estado con los datos de sessionStorage o con un array vacío
    const [users, setUsers] = useState<InputInterface[]>(() => {
@@ -18,9 +19,10 @@ export default function Home() {
   }, [users]);
   
   return (
-   <main className='w-full h-full flex flex-col gap-3 p-4 justify-center items-center'>
+   <main className='w-full h-full flex flex-col gap-3 p-4 justify-center items-center relative'>
     <Formulario setUsers={setUsers} users={users} />
     <ListCardUsuarios users={users} />
+ 
 
    </main> 
   )
