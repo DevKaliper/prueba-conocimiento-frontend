4ea7f10a2650e9c0d155f7959461e969aa32784e
Dev Kaliper <gxlthz@gmail.com>
2024-01-15 18:14:44 -0500

    Refactor user state management and session storage handling

--- app/page.tsx
+++ app/page.tsx
@@ -7,17 +7,36 @@ import { useState,useEffect } from 'react';
 
 
 export default function Home() {
-   // Inicializar el estado con los datos de sessionStorage o con un array vacío
-   const [users, setUsers] = useState<InputInterface[]>(() => {
-    const datosGuardados = sessionStorage.getItem('usuarios');
-    return datosGuardados ? JSON.parse(datosGuardados) : [];
-  });
 
-  // Guardar en sessionStorage cuando users cambie
-  useEffect(() => {
-    sessionStorage.setItem('usuarios', JSON.stringify(users));
-  }, [users]);
-  
+ const [users, setUsers] = useState<InputInterface[]>([]);
+
+ useEffect(() => {
+   // Esta función se ejecutará solo en el cliente
+   // es decir, cuando se renderice en el navegador y no en el servidor de Next.js (SSR)
+   const cargarUsuarios = () => {
+     const datosGuardados = sessionStorage.getItem('usuarios');
+     if (datosGuardados) {
+       setUsers(JSON.parse(datosGuardados));
+     }
+   };
+
+   if (typeof window !== "undefined") {
+     cargarUsuarios();
+   }
+ }, []);
+
+ useEffect(() => {
+   // Esto buscará el sessionStorage y guardará los datos  en caso de que existan
+
+   const guardarUsuarios = () => {
+     sessionStorage.setItem('usuarios', JSON.stringify(users));
+   };
+
+   if (typeof window !== "undefined") {
+     guardarUsuarios();
+   }
+ }, [users]);
+
   return (
    <main className='w-full h-full flex flex-col gap-3 p-4 justify-center items-center relative'>
     <Formulario setUsers={setUsers} users={users} />
